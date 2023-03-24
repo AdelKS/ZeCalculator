4d5b105d5e8b616efd9d269024610a3232e9d2f6
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2023-03-24 01:27:48 +0100

    test: MathWorld: add simple test

---
+++ test/math_world_test.cpp
@@ -0,0 +1,41 @@
+/****************************************************************************
+**  Copyright (c) 2023, Adel Kara Slimane <adel.ks@zegrapher.com>
+**
+**  This file is part of ZeCalculator.
+**
+**  ZeCalculators is free software: you may copy, redistribute and/or modify it
+**  under the terms of the GNU Affero General Public License as published by the
+**  Free Software Foundation, either version 3 of the License, or (at your
+**  option) any later version.
+**
+**  This file is distributed in the hope that it will be useful, but
+**  WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+**  General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+#include "zecalculator/builtin_function.h"
+#include "zecalculator/mathworld.h"
+#include <zecalculator/utils/parser.h>
+
+// testing specific headers
+#include <boost/ut.hpp>
+#include <zecalculator/test-utils/print-utils.h>
+
+using namespace zc;
+
+int main()
+{
+  using namespace boost::ut;
+
+  "simple test"_test = []()
+  {
+    expect(std::get<BuiltinFunction>(mathWorld.get_math_object("sqrt"))(4) == 2);
+  };
+
+  return 0;
+}
