e2928d99df554cb851bbfbe7cdfb627230c46b89
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2023-03-26 00:47:01 +0100

    MathWorld: move builtin unary functions array to same name header

--- include/zecalculator/builtin_unary_functions.h
+++ include/zecalculator/builtin_unary_functions.h
@@ -1,9 +1,52 @@
 #pragma once
 
-#include <functional>
+#include <string_view>
+#include <array>
+#include <cmath>
 
 namespace zc {
 
 using BuiltinUnaryFunction = double (*) (double);
+
+// we save the names along with the function pointers for convenience
+// we could save only the function pointers, and the names only in the inventory
+constexpr std::array<std::pair<std::string_view, BuiltinUnaryFunction>, 31> builtin_unary_functions = {{
+  {"cos",   static_cast<BuiltinUnaryFunction>(std::cos)},
+  {"sin",   static_cast<BuiltinUnaryFunction>(std::sin)},
+  {"tan",   static_cast<BuiltinUnaryFunction>(std::tan)},
+
+  {"acos",  static_cast<BuiltinUnaryFunction>(std::acos)},
+  {"asin",  static_cast<BuiltinUnaryFunction>(std::asin)},
+  {"atan",  static_cast<BuiltinUnaryFunction>(std::atan)},
+
+  {"cosh",  static_cast<BuiltinUnaryFunction>(std::cosh)},
+  {"sinh",  static_cast<BuiltinUnaryFunction>(std::sinh)},
+  {"tanh",  static_cast<BuiltinUnaryFunction>(std::tanh)},
+
+  {"ch",    static_cast<BuiltinUnaryFunction>(std::cosh)},
+  {"sh",    static_cast<BuiltinUnaryFunction>(std::sinh)},
+  {"th",    static_cast<BuiltinUnaryFunction>(std::tanh)},
+
+  {"acosh", static_cast<BuiltinUnaryFunction>(std::acosh)},
+  {"asinh", static_cast<BuiltinUnaryFunction>(std::asinh)},
+  {"atanh", static_cast<BuiltinUnaryFunction>(std::atanh)},
+
+  {"ach",   static_cast<BuiltinUnaryFunction>(std::acosh)},
+  {"ash",   static_cast<BuiltinUnaryFunction>(std::asinh)},
+  {"ath",   static_cast<BuiltinUnaryFunction>(std::atanh)},
+
+  {"sqrt",  static_cast<BuiltinUnaryFunction>(std::sqrt)},
+  {"log",   static_cast<BuiltinUnaryFunction>(std::log10)},
+  {"lg",    static_cast<BuiltinUnaryFunction>(std::log2)},
+  {"ln",    static_cast<BuiltinUnaryFunction>(std::log)},
+  {"abs",   static_cast<BuiltinUnaryFunction>(std::abs)},
+  {"exp",   static_cast<BuiltinUnaryFunction>(std::exp)},
+  {"floor", static_cast<BuiltinUnaryFunction>(std::floor)},
+  {"ceil",  static_cast<BuiltinUnaryFunction>(std::ceil)},
+  {"erf",   static_cast<BuiltinUnaryFunction>(std::erf)},
+  {"erfc",  static_cast<BuiltinUnaryFunction>(std::erfc)},
+  {"gamma", static_cast<BuiltinUnaryFunction>(std::tgamma)},
+  {"Γ",     static_cast<BuiltinUnaryFunction>(std::tgamma)},
+}};
 
 }
--- include/zecalculator/mathworld.h
+++ include/zecalculator/mathworld.h
@@ -2,12 +2,9 @@
 
 #include <zecalculator/builtin_unary_functions.h>
 
-#include <functional>
-#include <string_view>
+
 #include <unordered_map>
-#include <cmath>
 #include <variant>
-#include <optional>
 #include <string>
 
 namespace zc {
@@ -48,47 +45,6 @@ public:
   }
 
 protected:
-
-  // we save the names along with the function pointers for convenience
-  // we could save only the function pointers, and the names only in the inventory
-  static constexpr std::array<std::pair<std::string_view, BuiltinUnaryFunction>, 31> builtin_unary_functions = {{
-    {"cos",   static_cast<BuiltinUnaryFunction>(std::cos)},
-    {"sin",   static_cast<BuiltinUnaryFunction>(std::sin)},
-    {"tan",   static_cast<BuiltinUnaryFunction>(std::tan)},
-
-    {"acos",  static_cast<BuiltinUnaryFunction>(std::acos)},
-    {"asin",  static_cast<BuiltinUnaryFunction>(std::asin)},
-    {"atan",  static_cast<BuiltinUnaryFunction>(std::atan)},
-
-    {"cosh",  static_cast<BuiltinUnaryFunction>(std::cosh)},
-    {"sinh",  static_cast<BuiltinUnaryFunction>(std::sinh)},
-    {"tanh",  static_cast<BuiltinUnaryFunction>(std::tanh)},
-
-    {"ch",    static_cast<BuiltinUnaryFunction>(std::cosh)},
-    {"sh",    static_cast<BuiltinUnaryFunction>(std::sinh)},
-    {"th",    static_cast<BuiltinUnaryFunction>(std::tanh)},
-
-    {"acosh", static_cast<BuiltinUnaryFunction>(std::acosh)},
-    {"asinh", static_cast<BuiltinUnaryFunction>(std::asinh)},
-    {"atanh", static_cast<BuiltinUnaryFunction>(std::atanh)},
-
-    {"ach",   static_cast<BuiltinUnaryFunction>(std::acosh)},
-    {"ash",   static_cast<BuiltinUnaryFunction>(std::asinh)},
-    {"ath",   static_cast<BuiltinUnaryFunction>(std::atanh)},
-
-    {"sqrt",  static_cast<BuiltinUnaryFunction>(std::sqrt)},
-    {"log",   static_cast<BuiltinUnaryFunction>(std::log10)},
-    {"lg",    static_cast<BuiltinUnaryFunction>(std::log2)},
-    {"ln",    static_cast<BuiltinUnaryFunction>(std::log)},
-    {"abs",   static_cast<BuiltinUnaryFunction>(std::abs)},
-    {"exp",   static_cast<BuiltinUnaryFunction>(std::exp)},
-    {"floor", static_cast<BuiltinUnaryFunction>(std::floor)},
-    {"ceil",  static_cast<BuiltinUnaryFunction>(std::ceil)},
-    {"erf",   static_cast<BuiltinUnaryFunction>(std::erf)},
-    {"erfc",  static_cast<BuiltinUnaryFunction>(std::erfc)},
-    {"gamma", static_cast<BuiltinUnaryFunction>(std::tgamma)},
-    {"Γ",     static_cast<BuiltinUnaryFunction>(std::tgamma)},
-  }};
 
   struct string_hash
   {
