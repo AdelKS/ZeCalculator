ec27d8572ee411a784825b48ffe40f7e2178c504
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2023-03-27 00:07:49 +0200

    add GlobalConstant class
    Will implement global variables that will be available to
    any object within a MathWorld

---
+++ include/zecalculator/global_constant.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string_view>
+#include <array>
+#include <numbers>
+
+namespace zc {
+
+struct GlobalConstant
+{
+  double value = 0;
+};
+
+constexpr std::array<std::pair<std::string_view, double>, 5> builtin_global_variables =
+{{
+  {"math::pi", std::numbers::pi},
+  {"math::π",  std::numbers::pi},
+  {"physics::kB", 1.380649e-23},   // Blotzmann constant, SI units
+  {"physics::h",  6.62607015e-34}, // Plank constant, SI units
+  {"physics::c",  299792458},      // Speed of light in vacuum, SI units
+}};
+
+}
