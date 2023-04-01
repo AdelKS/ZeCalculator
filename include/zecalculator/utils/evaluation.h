67ce568593d71ad45b64c3a01db79d0d3c904b55
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2023-04-02 01:12:22 +0200

    evaluation: add evaluate() function that uses directly global_world

--- include/zecalculator/utils/evaluation.h
+++ include/zecalculator/utils/evaluation.h
@@ -8,4 +8,10 @@ namespace zc {
 /// @brief evaluates a syntax tree using a given math world
 tl::expected<double, EvaluationError> evaluate(const SyntaxTree& tree, const MathWorld& world);
 
+/// @brief evaluates a syntax tree using the global math world
+inline tl::expected<double, EvaluationError> evaluate(const SyntaxTree& tree)
+{
+  return evaluate(tree, global_world);
+}
+
 }
