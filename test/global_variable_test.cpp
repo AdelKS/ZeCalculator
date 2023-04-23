97796f0841445c21d4dd13ea4b46a26e98ae703c
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2023-04-23 02:43:39 +0200

    test: global variables

---
+++ test/global_variable_test.cpp
@@ -0,0 +1,66 @@
+/****************************************************************************
+**  Copyright (c) 2023, Adel Kara Slimane <adel.ks@zegrapher.com>
+**
+**  This file is part of ZeCalculator.
+**
+**  ZeCalculators is free software: you may copy, redistribute and/or modify it
+**  under the terms of the GNU Affero General Public License as published by the
+**  Free Software Foundation, either version 3 of the License, or (at your
+**  option) any later version.
+**
+**  This file is distributed in the hope that it will be useful, but
+**  WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+**  General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+#include <zecalculator/mathworld.h>
+#include <zecalculator/expression.h>
+
+// testing specific headers
+#include <boost/ut.hpp>
+#include <zecalculator/test-utils/print-utils.h>
+
+using namespace zc;
+
+int main()
+{
+  using namespace boost::ut;
+
+  "dependent expression"_test = []()
+  {
+    MathWorld world;
+    auto f = world.add("f", Function({"x", "y"}, "cos(math::pi * x) * y + k*g(x) + r"));
+    auto r = world.add("r", GlobalConstant());
+    world.add("k", GlobalVariable("3*g(3)"));
+    world.add("g", Function({"x"}, "sin(3 * math::pi * x) + r"));
+
+    double cpp_r = 3;
+    *r = cpp_r;
+
+    auto cpp_g = [&](double x){
+      return sin(3 * std::numbers::pi * x) + cpp_r;
+    };
+
+    auto cpp_k = [&]{
+      return 3 * cpp_g(3);
+    };
+
+    auto cpp_f = [&](double x, double y) {
+      return cos(std::numbers::pi * x) * y + cpp_k() * cpp_g(x) + cpp_r;
+    };
+
+    double x = 7, y = 8;
+
+    expect(f({x, y}).value() == cpp_f(x, y));
+
+    cpp_r = 10;
+    *r = cpp_r;
+
+    expect(f({x, y}).value() == cpp_f(x, y));
+  };
+}
