78643f5ad0c43b81c2628168a243ca035aa8f5cd
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2023-08-28 03:57:40 +0200

    test: function: make ast/rpn typed test

--- test/function_test.cpp
+++ test/function_test.cpp
@@ -23,6 +23,7 @@
 // testing specific headers
 #include <boost/ut.hpp>
 #include <zecalculator/test-utils/print-utils.h>
+#include <zecalculator/test-utils/structs.h>
 
 using namespace zc;
 
@@ -30,10 +31,12 @@ int main()
 {
   using namespace boost::ut;
 
-  "multi-parameter function evaluation"_test = []()
+  "multi-parameter function evaluation"_test = []<class StructType>()
   {
-    ast::MathWorld world;
-    auto f = ast::Function({"omega", "t"}, "cos(omega * t) + omega * t");
+    constexpr parsing::Type type = std::is_same_v<StructType, AST_TEST> ? parsing::AST : parsing::RPN;
+
+    MathWorld<type> world;
+    auto f = Function<type>({"omega", "t"}, "cos(omega * t) + omega * t");
 
     const double omega = 2;
     const double t = 3;
@@ -43,28 +46,34 @@ int main()
     const double expected_res = std::cos(omega * t) + omega * t;
 
     expect(res == expected_res);
-  };
 
-  "function evaluation shadowing a global constant"_test = []()
+  } | std::tuple<AST_TEST, RPN_TEST>{};
+
+  "function evaluation shadowing a global constant"_test = []<class StructType>()
   {
-    ast::MathWorld world;
-    world.add<GlobalConstant>("x", 2.0);
-    auto f = ast::Function({"x"}, "cos(x) + x");
+    constexpr parsing::Type type = std::is_same_v<StructType, AST_TEST> ? parsing::AST : parsing::RPN;
+
+    MathWorld<type> world;
+    world.add("x", GlobalConstant(2.0));
+    auto f = Function<type>({"x"}, "cos(x) + x");
 
     const double res = f({1.0}, world).value();
     const double expected_res = std::cos(1.0) + 1.0;
 
     expect(res == expected_res);
-  };
 
-  "function calling another function"_test = []()
+  } | std::tuple<AST_TEST, RPN_TEST>{};
+
+  "function calling another function"_test = []<class StructType>()
   {
-    ast::MathWorld world;
-    auto f1 = world.add<ast::Function>("f1").value();
-    auto f2 = world.add<ast::Function>("f2").value();
+    constexpr parsing::Type type = std::is_same_v<StructType, AST_TEST> ? parsing::AST : parsing::RPN;
 
-    *f1 = ast::Function({"x"}, "cos(x) + x + f2(2*x)");
-    *f2 = ast::Function({"x"}, "cos(x) + 2*x^2");
+    MathWorld<type> world;
+    auto f1 = world.add("f1", Function<type>()).value();
+    auto f2 = world.add("f2", Function<type>()).value();
+
+    *f1 = Function<type>({"x"}, "cos(x) + x + f2(2*x)");
+    *f2 = Function<type>({"x"}, "cos(x) + 2*x^2");
 
     expect(bool(*f1));
     expect(bool(*f2));
@@ -97,16 +106,19 @@ int main()
 
     if (bool(expected_res1))
       expect(std::fabs(expected_res1.value() - cpp_f1(x)) < 1e-11) << expected_res1.value() << " = " << cpp_f1(x);
-  };
+  } | std::tuple<AST_TEST, RPN_TEST>{};
 
-  "function overwrites"_test = []{
-    ast::MathWorld world;
+  "function overwrites"_test = []<class StructType>()
+  {
+    constexpr parsing::Type type = std::is_same_v<StructType, AST_TEST> ? parsing::AST : parsing::RPN;
+
+    MathWorld<type> world;
 
     // add a function named "f", note that the constant "my_constant" is only defined after
-    auto f = world.add<ast::Function>("f", ast::Function({"x"}, "x + my_constant + cos(math::pi)")).value();
+    auto f = world.add("f", Function<type>({"x"}, "x + my_constant + cos(math::pi)")).value();
 
     // add a global constant called "my_constant" with an initial value of 3.0
-    auto cst = world.add<GlobalConstant>("my_constant", 3.0).value();
+    auto cst = world.add("my_constant", GlobalConstant(3.0)).value();
 
     double cpp_cst = 3.0;
     auto cpp_f_1 = [&](double x)
@@ -122,7 +134,7 @@ int main()
     expect(f({1}).value() == cpp_f_1(1.0));
 
     // override expression and variable name of f
-    *f = ast::Function({"y", "z"}, "y + z + my_constant + g(y)");
+    *f = Function<type>({"y", "z"}, "y + z + my_constant + g(y)");
 
     auto cpp_g = [&](double z)
     {
@@ -134,19 +146,23 @@ int main()
       return y + z + cpp_cst + cpp_g(y);
     };
 
-    auto g = world.add<ast::Function>("g").value();
-    *g = ast::Function({"z"}, "2*z + my_constant");
+    auto g = world.add("g", Function<type>()).value();
+    *g = Function<type>({"z"}, "2*z + my_constant");
 
     expect(f({3, 4}).value() == cpp_f_2(3, 4));
-  };
 
-  "nested multi-variable functions"_test = []{
-    ast::MathWorld world;
+  } | std::tuple<AST_TEST, RPN_TEST>{};
+
+  "nested multi-variable functions"_test = []<class StructType>()
+  {
+    constexpr parsing::Type type = std::is_same_v<StructType, AST_TEST> ? parsing::AST : parsing::RPN;
+
+    MathWorld<type> world;
 
     // add a function named "f", note that the constant "my_constant" is only defined after
-    auto f = world.add<ast::Function>("f", ast::Function({"x", "y"}, "h(x, g(x, y)) + g(y, h(y, x))")).value();
-    world.add<ast::Function>("g", ast::Function({"a", "b"}, "h(a, a*b) + 3*a - b"));
-    world.add<ast::Function>("h", ast::Function({"c", "d"}, "c*d + c-d"));
+    auto f = world.add("f", Function<type>({"x", "y"}, "h(x, g(x, y)) + g(y, h(y, x))")).value();
+    world.add("g", Function<type>({"a", "b"}, "h(a, a*b) + 3*a - b"));
+    world.add("h", Function<type>({"c", "d"}, "c*d + c-d"));
 
     auto cpp_h = [](double c, double d) {
       return c*d + c-d;
@@ -163,31 +179,54 @@ int main()
     const double x = 5, y = 3;
 
     expect(f({x, y}).value() == cpp_f(x, y));
-  };
+  } | std::tuple<AST_TEST, RPN_TEST>{};
 
-  "function with dot in name"_test = []{
-    ast::MathWorld world;
+  "function with dot in name"_test = []<class StructType>()
+  {
+    constexpr parsing::Type type = std::is_same_v<StructType, AST_TEST> ? parsing::AST : parsing::RPN;
+
+    MathWorld<type> world;
 
     // add a function named "f", note that the constant "my_constant" is only defined after
-    auto fx = world.add("f.x", ast::Function({"x"}, "1 + x")).value();
-    auto fy = world.add("f.y", ast::Expression("2.0 + f.x(1)")).value();
+    auto fx = world.add("f.x", Function<type>({"x"}, "1 + x")).value();
+    auto fy = world.add("f.y", Expression<type>("2.0 + f.x(1)")).value();
 
     expect(fx({1}) == 2.0);
     expect(fy() == 4.0);
-  };
 
-  "calling function without arguments"_test = []{
-    ast::MathWorld world;
+  } | std::tuple<AST_TEST, RPN_TEST>{};
+
+  "calling function without arguments"_test = []<class StructType>()
+  {
+    constexpr parsing::Type type = std::is_same_v<StructType, AST_TEST> ? parsing::AST : parsing::RPN;
+
+    MathWorld<type> world;
 
     // add a function named "f", note that the constant "my_constant" is only defined after
-    world.add("f", ast::Function({"x", "y"}, "1 + x + y")).value();
-    auto expr = world.add("val", ast::Expression("1 + f(1)")).value();
+    world.add("f", Function<type>({"x", "y"}, "1 + x + y")).value();
+    auto expr = world.add("val", Expression<type>("1 + f(1)")).value();
 
-    expect(expr()
-           == tl::unexpected(eval::Error::mismatched_fun_args(
-             ast::node::Function(parsing::tokens::Text("f", 4, 1),
-                                 {ast::node::Number(1.0, parsing::tokens::Text("1", 6, 1))}))))
-      << expr();
-  };
+    [[maybe_unused]] const auto& parsing = expr->get_parsing().value();
+
+    if constexpr (type == parsing::AST)
+    {
+      expect(expr()
+             == tl::unexpected(eval::Error::mismatched_fun_args(
+               ast::node::Function(parsing::tokens::Text("f", 4, 1),
+                                   {ast::node::Number(1.0, parsing::tokens::Text("1", 6, 1))}))))
+        << expr();
+    }
+    else
+    {
+      // error message in postfix notation isn't accurate, because the expression is translated to
+      // "1 + f(1)" -> "1, 1, f, +" and 'f' will pick the first two values, then '+' ends up with just one
+      // value
+      expect(expr()
+             == tl::unexpected(eval::Error::mismatched_fun_args(parsing::tokens::Operator('+', 2))))
+        << expr();
+    }
+
+
+  } | std::tuple<AST_TEST, RPN_TEST>{};
 
 }
